21523fcc967cdecae9869bb92b57ffe19910f820
wuzihao <wuzihao051119@outlook.com>
2022-02-14 10:45:07 +0800

    ngca

--- ngca.py
+++ ngca.py
@@ -117,4 +117,4 @@ def view_data():
     return render_template("image.html")
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8080, debug = True)
+    app.run(host = "0.0.0.0", port = 8080)
